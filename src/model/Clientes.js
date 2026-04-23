678a3faad8fe172346817f52af1fec96f3e8ef94
wagnermarques <wagnerdocri@gmail.com>
2026-04-22 22:34:42 -0300

    add pastas de construção e começamos a desenvolver o app.js e criamos o construtor dos clientes

---
+++ src/model/Clientes.js
@@ -0,0 +1,11 @@
+class Client{
+    constructor(id, nome, telefone, endereco){
+        this.id = id;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.endereco = endereco;
+
+    }
+
+}
+module.exports = Cliente;
