0aff9e3ec359274aabcc8c06d80aae16df398e26
wagnermarques <wagnerdocri@gmail.com>
2026-04-29 22:16:43 -0300

    adicionei um clienteRoutes.js e atualizei meu app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,34 @@
 const express = require("express")
+const clienteRoutes = ("./routes/clienteRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+//Rota raiz:
+app.get("/", (req, res) => {
+    res.json({
+        mensagem: "API de clientes",
+        versao: "1.0.0",
+        endpoints: {
+            listarTodos: "GET /clientes",
+            buscarPorId: "GET /clientes/:id",
+        }
+    })
+
+})
+app.use("/", "/clientes", clienteRoutes);
+
+app.use((req, res) =>{
+    res.status(404).json({
+        sucesso: false,
+        mensagem: "Rota não encontrada",
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`servidor rodando na porta ${PORT}`)
+})
+
+module.exports = app;
